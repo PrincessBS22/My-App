c6e60f0aa311ca76751e0f114a7151c0fcbd8b79
Bria <shaverb@purdue.edu>
2025-01-23 17:07:43 -0500

    lab 4

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,50 @@ import About from "./components/about";
 import Card1 from "./components/card1";
 import Card2 from "./components/card2";
 import Navbar from "./components/navbar";
+import img_1 from "./assets/20231121_145928.jpg";
+import img_2 from "./assets/maxresdefault.jpg";
+import Card from "./components/card";
+import Wrapper from "./components/wrapper";
 
 const App = () => {
-  return(
+  const profiles = [{
+    img: img_1,
+    name: 'Jane Doe',
+    title: 'Engineer',
+    email: 'rar.gmail.com'
+  }, {
+    img: img_2,
+    name: 'John Smith',
+    title: 'Social Media Manager',
+    email: 'json.gmail.com'
+  }]
+
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  return (
     <>
-    <header>
-      <Navbar/>
-    </header>
-    <main>
-      <div className="section">
-        <div className="container">
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Wrapper>
           <h1>Profiler App</h1>
-        </div>
-      </div>
-      <div className="section">
-        <div className="container">
-          <About/>
-        </div>
-      </div>
-      <div className="section">
-        <div className="container">
+          <button onClick={handleClick}>
+            {clicked ? "Clicked" : "Click me"}
+          </button>
+        </Wrapper>
+        <Wrapper>
+          <About />
+        </Wrapper>
+        <Wrapper>
           <div className="profile-cards">
-              <Card1/>
-              <Card2/>
+            {profiles.map(profile => <Card key={profile.email} {...profile} />)}
           </div>
-        </div>
-      </div>
-    </main>
+        </Wrapper>
+      </main>
     </>
   )
 }
